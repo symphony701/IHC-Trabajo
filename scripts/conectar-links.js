611e2a1350b7e2166d20ce495e9215d3bbfe6d52
Symphony <rnsebastian701@gmail.com>
2021-06-20 00:00:54 -0500

    espero que sea lo ultimo xd

---
+++ scripts/conectar-links.js
@@ -0,0 +1,13 @@
+const logo = document.querySelector("#logo")
+const flecha = document.querySelector("#flecha")
+logo.addEventListener("click", function(){
+    setTimeout(function(){
+        window.open("./../index.html","_self")
+    }, 200);
+});
+
+flecha.addEventListener("click", function(){
+    setTimeout(function(){
+        window.open("./../index.html","_self")
+    }, 200);
+});
